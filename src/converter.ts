9af4db6cb0ac7f3beae18ebc24b74ee1f1cb8614
montmorill <montmorill@outlook.com>
2026-09-02 17:44:27 +0800

    chore: update import paths to remove file extensions and align with ESM module format

--- src/converter.ts
+++ src/converter.ts
@@ -7,8 +7,8 @@ import type {
   Polygon,
   Position,
 } from 'geojson'
-import type { Options, Origin } from './types.js'
-import { explode } from './utils.js'
+import type { Options, Origin } from './types'
+import { explode } from './utils'
 
 export type ConverterFn = (geom: any, res: number, origin: Origin, opt: Options) => string[]
 
